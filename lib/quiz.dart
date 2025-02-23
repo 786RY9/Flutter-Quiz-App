526d4bc7e69e754a2c8e2bf9db2ecf6e73f60da7
Rashid Yaseen <ryaseen.bscs22seecs@seecs.edu.pk>
2025-02-23 16:12:17 +0500

    Final commit

--- lib/quiz.dart
+++ lib/quiz.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:quiz_app/data/questions.dart';
+import 'package:quiz_app/data/results_screen.dart';
 import 'package:quiz_app/questions_screen.dart';
 import 'package:quiz_app/start_screen.dart';
 
@@ -13,30 +15,73 @@ class Quiz extends StatefulWidget {
 }
 
 class _Quiz extends State<Quiz> {
-  Widget activeScreen = StartScreen();
+  List<String> selectedAnswers = [];
+  var activeScreen = 'start-screen';
+
+  // Widget? activeScreen;
+
+  // @override
+  // void initState() {
+  //   activeScreen = StartScreen(switchScreen);
+  //   super.initState();
+  // }
 
   void switchScreen() {
     setState(() {
-      activeScreen = QuestionsScreen();
+      // activeScreen = QuestionsScreen();
+      activeScreen = 'questions-screen';
+    });
+  }
+
+  void chooseAnswer(String answer) {
+    selectedAnswers.add(answer);
+
+    if (selectedAnswers.length == questions.length) {
+      setState(() {
+        activeScreen = 'results-screen';
+      });
+    }
+  }
+
+  void restartQuiz() {
+    setState(() {
+      selectedAnswers = [];
+      activeScreen = 'questions-screen';
     });
   }
 
   @override
   Widget build(context) {
+    // final screenWidget = activeScreen == 'start-screen'
+    //               ? StartScreen(switchScreen)
+    //               : const QuestionsScreen();
+
+    Widget screenWidget = StartScreen(switchScreen);
+
+    if (activeScreen == 'questions-screen') {
+      screenWidget = QuestionsScreen(onSelectAnswer: chooseAnswer);
+    }
+    if (activeScreen == 'results-screen') {
+      screenWidget = ResultsScreen(
+        chosenAnswers: selectedAnswers,
+        onRestart: restartQuiz,
+      );
+    }
+
     return MaterialApp(
       home: Scaffold(
         body: Container(
           decoration: const BoxDecoration(
             gradient: LinearGradient(
               colors: [
-                Color.fromARGB(255, 24, 6, 127),
-                Color.fromARGB(255, 249, 144, 38),
+                Color.fromARGB(255, 14, 14, 134),
+                Color.fromARGB(255, 189, 99, 9),
               ],
               begin: Alignment.topLeft,
               end: Alignment.bottomRight,
             ),
           ),
-          child: activeScreen,
+          child: screenWidget,
         ),
       ),
     );
--- lib/start_screen.dart
+++ lib/start_screen.dart
@@ -1,7 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class StartScreen extends StatelessWidget {
-  const StartScreen({super.key});
+  const StartScreen(this.startQuiz, {super.key});
+
+  final void Function() startQuiz;
 
   @override
   Widget build(context) {
@@ -10,21 +13,24 @@ class StartScreen extends StatelessWidget {
         mainAxisSize: MainAxisSize.min,
 
         children: [
-          Image.asset('assets/images/quiz-logo.png', width: 300,
-          color: const Color.fromARGB(117, 6, 35, 122),
+          Image.asset(
+            'assets/images/quiz-logo.png',
+            width: 300,
+            color: const Color.fromARGB(255, 37, 2, 107),
           ),
           SizedBox(height: 80),
-          const Text(
+          Text(
             'Learn Flutter the Fun Way',
-            style: TextStyle(color: Colors.white, fontSize: 24),
+            style:  GoogleFonts.lato(
+               color: const Color.fromARGB(255, 54, 9, 143),fontSize: 24,
+               fontWeight: FontWeight.bold,
+            ),
           ),
           SizedBox(height: 30),
           OutlinedButton.icon(
             style: OutlinedButton.styleFrom(foregroundColor: Colors.white),
-            onPressed: () {},
-            icon: const Icon(Icons.arrow_right_alt,
-            color: Colors.white,
-            ),
+            onPressed: startQuiz,
+            icon: const Icon(Icons.arrow_right_alt, color: Colors.white),
             label: const Text('Start Quiz'),
           ),
         ],
